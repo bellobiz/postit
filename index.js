dcc079c4e2eb61441aef0b8b8d826390cfb7593a
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-18 15:45:53 +0100

    include script in file

---
+++ index.js
@@ -0,0 +1,23 @@
+const group_navItem = document.querySelector("li.cancel");
+const close = document.querySelector(".col-lg-4");
+const chatSection = document.querySelector(".col-lg-8");
+const cancel = document.querySelector("a.active");
+
+const openBar = () => {
+    close.style.flex = '0 0 33.333333%';
+    close.style.maxWidth = '33.333333%';
+    chatSection.style.flex = '0 0 66.666667%';
+    chatSection.style.maxWidth = '66.666667%';
+    group_navItem.style.color = '#fff';
+}
+
+const closeBar = () => {
+    close.style.flex = '';
+    close.style.maxWidth = '';
+    chatSection.style.flex = '';
+    chatSection.style.maxWidth = '';
+    group_navItem.style.color = '';
+}
+
+cancel.addEventListener('click', openBar);
+group_navItem.addEventListener('click', closeBar);
