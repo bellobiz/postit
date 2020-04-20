1259ae75babfd4d46e432040d0a95e0c9035d991
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-20 07:41:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,80 @@
 const group_navItem = document.querySelector("li.cancel");
 const close = document.querySelector(".col-lg-4");
 const chatSection = document.querySelector(".col-lg-8");
-const textArea = document.querySelector('.textarea');
-const send = document.querySelector('.far.fa-paper-plane');
+const textArea = document.querySelectorAll("textarea");
+const send = document.querySelector("i.far.fa-paper-plane");
 const cancel = document.querySelector("a.active");
-const searchBar = document.querySelector("#search-bar")
-const contactsBar = document.querySelector('#contacts-bar');
-const navItem1 = document.querySelector('.nav-item1');
-const navItem2 = document.querySelector('.nav-item2');
-const navItem3 = document.querySelector('.nav-item3');
+const searchBar = document.querySelector("#search-bar");
+const contactsBar = document.querySelector("#contacts-bar");
+const navItem1 = document.querySelector(".nav-item1");
+const navItem2 = document.querySelector(".nav-item2");
+const navItem3 = document.querySelector(".nav-item3");
 
-const openBar = () => {
-    close.style.flex = '0 0 33.333333%';
-    close.style.maxWidth = '33.333333%';
-    chatSection.style.msFlex = '0 0 66.666667%';
-    chatSection.style.flex = '0 0 66.666667%';
-    chatSection.style.maxWidth = '66.666667%';
-    cancel.style.color = '#a8a4a4';
-    textArea.style.marginLeft = '33.333333%';
+for (i = 0; i <= textArea.length; i++) {
+  textArea[0].style.width = "33.33333";
 }
+const openBar = () => {
+  close.style.msFlex = "0 0 33.333333%";
+  close.style.flex = "0 0 33.333333%";
+  close.style.maxWidth = "33.333333%";
+  chatSection.style.msFlex = "0 0 66.666667%";
+  chatSection.style.flex = "0 0 66.666667%";
+  chatSection.style.maxWidth = "66.666667%";
+  cancel.style.color = "#a8a4a4";
+  for (i = 0; i <= textArea.length; i++) {
+    textArea[0].style.width = "66.666667%";
+    textArea[0].style.marginLeft = "33.333333%";
+  }
+  send.style.display = "none";
+};
 
 const closeBar = () => {
-    close.style.flex = '';
-    close.style.maxWidth = '';
-    chatSection.style.flex = '';
-    chatSection.style.maxWidth = '';
-    cancel.style.color = '#fff';
-    textArea.style.marginLeft = '';
-}
+  close.style.flex = "";
+  close.style.maxWidth = "";
+  chatSection.style.flex = "";
+  chatSection.style.maxWidth = "";
+  cancel.style.color = "#fff";
+};
 
 const altCloseBar = () => {
-    close.style.maxWidth = ''; 
-    cancel.style.color = '#fff';
-}
+  close.style.maxWidth = "";
+  cancel.style.color = "#fff";
+  for (i = 0; i <= textArea.length; i++) {
+    textArea[0].style.width = "";
+    textArea[0].style.marginLeft = "";
+  }
+};
 
 const openSearch = () => {
-    searchBar.style.display = 'none';
-    contactsBar.style.display = 'block';
-    navItem1.style.display = 'none';
-    navItem2.style.display = 'none';
-    navItem3.style.display = 'none';
-    contactsBar.style.marginRight = 'auto';
-}
+  searchBar.style.display = "none";
+  contactsBar.style.display = "block";
+  navItem1.style.display = "none";
+  navItem2.style.display = "none";
+  navItem3.style.display = "none";
+  contactsBar.style.marginRight = "auto";
+};
 
 const closeSearch = () => {
-    searchBar.style.display = '';
-    contactsBar.style.display = '';
-    navItem1.style.display = '';
-    navItem2.style.display = '';
-    navItem3.style.display = '';
-    contactsBar.style.marginLeft = '';
-}
+  searchBar.style.display = "";
+  contactsBar.style.display = "";
+  navItem1.style.display = "";
+  navItem2.style.display = "";
+  navItem3.style.display = "";
+  contactsBar.style.marginLeft = "";
+};
 
 const altCloseSearch = () => {
-    searchBar.style.display = '';
-    contactsBar.style.display = '';
-    navItem1.style.display = '';
-    navItem2.style.display = '';
-    navItem3.style.display = '';
-    contactsBar.style.marginLeft = '';
-}
+  searchBar.style.display = "";
+  contactsBar.style.display = "";
+  navItem1.style.display = "";
+  navItem2.style.display = "";
+  navItem3.style.display = "";
+  contactsBar.style.marginLeft = "";
+};
 
-searchBar.addEventListener('click', openSearch);
-contactsBar.addEventListener('blur', closeSearch)
-chatSection.addEventListener('click', altCloseBar);
-chatSection.addEventListener('click', altCloseSearch)
-cancel.addEventListener('click', openBar);
-group_navItem.addEventListener('click', closeBar);
+searchBar.addEventListener("click", openSearch);
+contactsBar.addEventListener("blur", closeSearch);
+chatSection.addEventListener("click", altCloseBar);
+chatSection.addEventListener("click", altCloseSearch);
+cancel.addEventListener("click", openBar);
+group_navItem.addEventListener("click", closeBar);
