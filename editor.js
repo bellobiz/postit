69ce53fe6f1008e32ffdddebd5dc83706cec473a
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-21 23:27:46 +0100

    Add files via upload

---
+++ editor.js
@@ -0,0 +1,40 @@
+const lightMode = document.querySelector('.light-mode');
+const darkMode = document.querySelector('.dark-mode');
+const section = document.querySelectorAll('section');
+const navItem = document.querySelector(".nav-item1");
+const textAreaSection = document.querySelectorAll('textarea.editor');
+const removeEditor = document.querySelector('span.material-icons');
+
+const showLightMode = () => {
+    for (i = 0; i <= textAreaSection.length; i++) {
+        textAreaSection[i].style.backgroundColor = '#fff';
+        textAreaSection[i].style.color = '#444';
+    }
+}
+
+const showDarkMode = () => {
+    for (i = 0; i <= textAreaSection.length; i++) {
+        textAreaSection[i].style.backgroundColor = '';
+        textAreaSection[i].style.color = '';
+    }
+}
+
+const showEditor = () => {
+    navItem.style.color = '#a8a4a4';
+    for (i = 0; i <= section.length; i++) {
+        section[i].style.display = 'block';
+        section[i].style.width = '';
+    }
+}
+
+const delEditor = () => {
+    navItem.style.color = '';
+    for (i = 0; i <= section.length; i++) {
+        section[i].style.display = '';
+    }
+}
+
+lightMode.addEventListener('click', showLightMode);
+darkMode.addEventListener('click', showDarkMode);
+navItem.addEventListener('click', showEditor);
+removeEditor.addEventListener('click', delEditor);
