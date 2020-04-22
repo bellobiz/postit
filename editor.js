5bcb44b7b8c99c5bed1ddb36519ffedaa9ce4c99
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-23 00:52:52 +0100

    format file

--- editor.js
+++ editor.js
@@ -4,6 +4,7 @@ const section = document.querySelectorAll('section');
 const navItem = document.querySelector(".nav-item1");
 const textAreaSection = document.querySelectorAll('textarea.editor');
 const removeEditor = document.querySelector('span.material-icons');
+const chatBox = document.querySelectorAll("textarea.textarea");
 
 const showLightMode = () => {
     textAreaSection[0].style.backgroundColor = '#fff';
@@ -17,12 +18,14 @@ const showDarkMode = () => {
 
 const showEditor = () => {
     section[0].style.display = 'block';
-    navItem.style.color = "#a8a4a4"
+    navItem.style.color = "#a8a4a4";
+    chatBox[0].style.display = 'none';
 }
 
 const delEditor = () => {
     section[0].style.display = '';
-    navItem.style.color = "#fff"
+    navItem.style.color = "#fff";
+    chatBox[0].style.display = 'block';
 }
 
 const toggleEditor = () => {
