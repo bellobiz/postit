870b613a7a0d7f010e29f57200dc24931f13903c
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-24 17:21:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,92 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+const closeSideBar = document.querySelector("li.cancel");
+const closeIcon = document.querySelector("i.material-icons");
+const sideBar = document.querySelector(".col-lg-4");
+const chatSection = document.querySelector(".col-lg-8");
+const textArea = document.querySelectorAll("textarea.textarea");
+const sendIcon = document.querySelector("i.far.fa-paper-plane");
+const activeGroup = document.querySelector(".nav-item2");
+const contactsSearchBarIcon = document.querySelector("#search-bar");
+const contactsSearchBar = document.querySelector("#contacts-bar");
+const navItem1 = document.querySelector(".nav-item1");
+const navItem2 = document.querySelector(".nav-item2");
+const navItem3 = document.querySelector(".nav-item3");
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    var div = this.parentElement;
-    div.style.display = "none";
-  };
-}
+const openBar = () => {
+    sideBar.style.flex = "0 0 33.333333%";
+    sideBar.style.maxWidth = "33.333333%";
+    chatSection.style.flex = "0 0 100%";
+    chatSection.style.maxWidth = "100%";
+    activeGroup.style.color = "#a8a4a4";
+    for (let i = 0; i <= textArea.length; i++) {
+        textArea[0].style.width = "60%";
+        textArea[0].style.marginLeft = "36.6666667%";
+    }
+    sendIcon.style.display = "none";
+    closeSideBar.style.display = 'block';
+};
 
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.style.backgroundColor = "#fff";
-  li.style.borderRadius = "4px";
-  li.style.padding = "5px";
-  li.style.overflow = "auto";
-  li.style.maxHeight = "200px";
-  li.style.marginLeft = "40%";
-  li.style.marginRight = "40px";
-  li.style.fontSize = "medium";
-  li.style.fontFamily = "Roboto Condensed";
-  li.appendChild(t);
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+const closeBar = () => {
+    sideBar.style.flex = "";
+    sideBar.style.maxWidth = "";
+    chatSection.style.flex = "";
+    chatSection.style.maxWidth = "";
+    activeGroup.style.color = "#fff";
+    for (let i = 0; i <= textArea.length; i++) {
+        textArea[0].style.width = "";
+        textArea[0].style.marginLeft = "";
+    }
+    sendIcon.style.display = "";
+    sendIcon.style.left = "";
+    closeSideBar.style.display = 'none';
+    closeIcon.style.display = 'none';
+};
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+const altCloseBar = () => {
+    sideBar.style.maxWidth = "";
+    sideBar.style.flex = "";
+    activeGroup.style.color = "#fff";
+    for (let i = 0; i <= textArea.length; i++) {
+        textArea[0].style.width = "";
+        textArea[0].style.marginLeft = "";
+    }
+    sendIcon.style.display = "";
+    sendIcon.style.left = "";
+};
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
-}
+const openSearch = () => {
+    contactsSearchBarIcon.style.display = "none";
+    contactsSearchBar.style.display = "block";
+    navItem1.style.display = "none";
+    navItem2.style.display = "none";
+    navItem3.style.display = "none";
+    contactsSearchBar.style.marginRight = "auto";
+};
+
+const closeSearch = () => {
+    contactsSearchBarIcon.style.display = "";
+    contactsSearchBar.style.display = "";
+    navItem1.style.display = "";
+    navItem2.style.display = "";
+    navItem3.style.display = "";
+    contactsSearchBar.style.marginLeft = "";
+};
+
+const altCloseSearch = () => {
+    contactsSearchBarIcon.style.display = "";
+    contactsSearchBar.style.display = "";
+    navItem1.style.display = "";
+    navItem2.style.display = "";
+    navItem3.style.display = "";
+    contactsSearchBar.style.marginLeft = "";
+};
+
+const displayDynamicElements = () => {
+    contactsSearchBarIcon.addEventListener("click", openSearch);
+    contactsSearchBar.addEventListener("blur", closeSearch);
+    chatSection.addEventListener("click", altCloseBar);
+    chatSection.addEventListener("click", altCloseSearch);
+    activeGroup.addEventListener("click", openBar);
+    closeSideBar.addEventListener("click", closeBar);
+};
+
+displayDynamicElements();
