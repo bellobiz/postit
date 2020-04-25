8f8cc0c31422d0fae995b8b8fa5944a41a7e8388
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-25 20:33:38 +0100

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,53 +1,53 @@
-const lightMode = document.querySelector('.light-mode');
-const darkMode = document.querySelector('.dark-mode');
-const section = document.querySelectorAll('section');
-const navItem = document.querySelector(".nav-item1");
-const textAreaSection = document.querySelectorAll('textarea.editor');
-const removeEditor = document.querySelector('span.material-icons');
-const chatBox = document.querySelectorAll("textarea.textarea");
-
-function myFunction() {
-    var copyText = document.getElementById("my_Input");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copied: " + copyText.value;
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-function outFunc() {
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copy to clipboard";
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
 }
 
-const showLightMode = () => {
-    textAreaSection[0].style.backgroundColor = '#fff';
-    textAreaSection[0].style.color = '#444';
-}
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.style.backgroundColor = "#fff";
+  li.style.borderRadius = "4px";
+  li.style.padding = "5px";
+  li.style.overflow = "auto";
+  li.style.maxHeight = "300px";
+  li.style.marginLeft = "40%";
+  li.style.marginRight = "40px";
+  li.style.fontSize = "medium";
+  li.style.fontFamily = "Roboto Condensed";
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
-const showDarkMode = () => {
-    textAreaSection[0].style.backgroundColor = '';
-    textAreaSection[0].style.color = '';
-}
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-const showEditor = () => {
-    section[0].style.display = 'block';
-    navItem.style.color = "#a8a4a4";
-    chatBox[0].style.display = 'none';
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
 }
-
-const delEditor = () => {
-    section[0].style.display = '';
-    navItem.style.color = "#fff";
-    chatBox[0].style.display = 'block';
-}
-
-const toggleEditor = () => {
-    lightMode.addEventListener('click', showLightMode);
-    darkMode.addEventListener('click', showDarkMode);
-    navItem.addEventListener('click', showEditor);
-    removeEditor.addEventListener('click', delEditor);
-}
-
-toggleEditor()
