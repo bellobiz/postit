8cdf78f4448d83c44d749dad721387ae599d7336
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-28 23:05:08 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,6 +3,9 @@ const settingSection = document.querySelector('#body');
 const remove_Setting = document.querySelector('i.icon.material-icons');
 const tabBtn = document.querySelectorAll('.tab');
 const tab = document.querySelectorAll('.tabShow');
+const form = document.querySelectorAll('form');
+const sendFormButton = document.querySelector('.far.fa-paper-plane');
+const RemoveGithubIcon = document.querySelector('.github.fab.fa-github');
 
 const tabs = panelIndex => {
     tab.forEach(node => {
@@ -15,11 +18,17 @@ tabs(0);
 const showSetting = () => {
     settingSection.style.display = 'block';
     nav_Item.style.color = "#a8a4a4";
+    form[0].style.display = 'none';
+    sendFormButton.style.display = 'none';
+    RemoveGithubIcon.style.display = 'none';
 }
 
 const removeSetting = () => {
     settingSection.style.display = '';
     nav_Item.style.color = "#fff";
+    form[0].style.display = 'block';
+    sendFormButton.style.display = 'block';
+    RemoveGithubIcon.style.display = 'block';
 }
 
 const toggleSetting = () => {
