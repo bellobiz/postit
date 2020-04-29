d84d19e25c1e60ee254f07db40e4c2fd7cef5a5c
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-04-29 17:33:02 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,8 +1,8 @@
 const nav_Item = document.querySelector("#nav-item3");
 const nav__1 = document.querySelector("#nav-item1");
 const nav__2 = document.querySelector("#nav-item2");
-// const editor__mode = document.querySelectorAll("#editor-area");
 const settingSection = document.querySelector('#body');
+const textarea_ = document.querySelectorAll('textarea.textarea');
 const cancelSideBarSection = document.querySelector('li.cancel');
 const remove_Setting = document.querySelector('i.icon.material-icons');
 const tabBtn = document.querySelectorAll('.tab');
@@ -23,36 +23,26 @@ tabs(0);
 const showSetting = () => {
     settingSection.style.display = 'block';
     nav_Item.style.color = "#a8a4a4";
-    for (let i = 0; i <= textArea.length; i++) {
-        textArea[0].style.width = "60%";
-        textArea[0].style.marginLeft = "36.6666667%";
-        form[0].style.display = 'none';
-    }
     sendFormButton.style.display = 'none';
     RemoveGithubIcon.style.display = 'none';
     cancelSideBarSection.style.display = 'none';
     remove_Setting.style.color = '#000';
     side_bar.style.display = 'none';
-    // editor__mode.style.display = 'none';
+    side_bar.style.width = '0px';
     nav__2.style.cursor = 'not-allowed';
-    // nav__1.style.cursor = 'not-allowed';
+    nav__1.style.cursor = 'not-allowed';
+    textarea_[0].style.marginLeft = '0%'
 }
 
 const removeSetting = () => {
     settingSection.style.display = '';
     nav_Item.style.color = "#fff";
-    for (let i = 0; i <= textArea.length; i++) {
-        textArea[0].style.width = "";
-        textArea[0].style.marginLeft = "";
-        form[0].style.display = '';
-    }
     sendFormButton.style.display = '';
     RemoveGithubIcon.style.display = '';
     cancelSideBarSection.style.display = '';
     side_bar.style.display = '';
-    // editor__mode.style.display = 'none';
     nav__2.style.cursor = '';
-    // nav__1.style.cursor = 'not-allowed';
+    nav__1.style.cursor = 'not-allowed';
 }
 
 const toggleSetting = () => {
