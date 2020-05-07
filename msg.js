f39b01df4ec3a243bf1e45a8625a4e59f3a1efce
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-05-07 07:24:21 +0100

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,26 +1,26 @@
-var myNodelist = document.getElementsByTagName("LI");
+let myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("❌");
+for (i of myNodelist) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("❌");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
-var close = document.getElementsByClassName("close");
+let close = document.getElementsByClassName("close");
 var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
+for (i of close) {
+  close[i].onclick = () => {
     var div = this.parentElement;
     div.style.display = "none";
   };
 }
 
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === "") {
     alert("You must write something!\n\n1. Windows/Linux\nFirefox - Alt + Shift + S\n2. Mac\nFirefox - Control + Alt + S\n3. Windows/Linux/Mac\nOpera - Alt (Option) + S\n\nAlt (Windows/Linux) or Option (Mac/Linux) Key works the same way.");
@@ -29,15 +29,15 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("❌");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("❌");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     };
   }
