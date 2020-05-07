7ab3129fe170808f205e0d78db9a4e6db0e0cc3c
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-05-07 18:14:34 +0100

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,43 +1,52 @@
-let myNodelist = document.getElementsByTagName("LI");
-var i;
+let myNodelist = document.querySelectorAll("li");
+
+let i;
+let span;
+let txt;
 for (i of myNodelist) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("❌");
+  span = document.createElement("span");
+  txt = document.createTextNode("❌");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
-let close = document.getElementsByClassName("close");
-var i;
-for (i of close) {
-  close[i].onclick = () => {
-    var div = this.parentElement;
+let close = document.querySelectorAll(".close");
+
+let i_;
+let div;
+for (i_ of close) {
+  close[i_].onclick = () => {
+    div = this.parentElement;
     div.style.display = "none";
   };
 }
 
-function newElement() {
+let newElement = () => {
   let li = document.createElement("li");
-  let inputValue = document.getElementById("myInput").value;
+  let inputValue = document.querySelector("#myInput").value;
   let t = document.createTextNode(inputValue);
+  let span;
+  let txt;
   li.appendChild(t);
   if (inputValue === "") {
     alert("You must write something!\n\n1. Windows/Linux\nFirefox - Alt + Shift + S\n2. Mac\nFirefox - Control + Alt + S\n3. Windows/Linux/Mac\nOpera - Alt (Option) + S\n\nAlt (Windows/Linux) or Option (Mac/Linux) Key works the same way.");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.querySelector("#myUL").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.querySelector("#myInput").value = "";
 
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("❌");
+  span = document.createElement("span");
+  txt = document.createTextNode("❌");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement;
+  let counter;
+  let div;
+  for (counter of close) {
+    close[counter].onclick = () => {
+      div = this.parentElement;
       div.style.display = "none";
     };
   }
