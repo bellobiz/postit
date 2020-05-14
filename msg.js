e293aa08e45ee893c489c8fad24c2456b24dc2e8
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-05-14 18:00:47 +0100

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,43 +1,56 @@
-var myNodelist = document.querySelectorAll("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("❌");
+let myNodelist; 
+let span;
+let txt
+let iter1;
+let close; 
+let iter2;
+let div;
+let newElement;
+myNodelist = document.querySelectorAll("LI");
+for (iter1 of myNodelist) {
+  span = document.createElement("SPAN");
+  txt = document.createTextNode("❌");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    var div = this.parentElement;
+close = document.getElementsByClassName("close");
+for (iter2 of close) {
+  close[i].onclick = () => { 
+    div = this.parentElement;
     div.style.display = "none";
   };
 }
 
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+newElement = () => {
+  let li;
+  let inputValue;
+  let t;
+  let span;
+  let txt;
+  let div;
+  let iter;
+  li = document.createElement("li");
+  inputValue = document.querySelector("#myInput").value;
+  t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === "") {
     alert("alerYou must type something then send! 😢\n\n💻 Try upgrading your browser and if the AltGr or Insert key still not working, try the following keys.\n\n1. Windows/Linux\nFirefox - AltGr + Shift + S\n2. Mac\nFirefox - Control + AltGr + S\n3. Windows/Linux/Mac\nOpera - AltGr (Option) + S\n\nAltGr (Windows/Linux) or Option (Mac/Linux) Key works the same way.\nAltGr is the Alt right key on your keyboard.");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.querySelector("#myUL").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.querySelector("#myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("❌");
+  span = document.createElement("SPAN");
+  txt = document.createTextNode("❌");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
+  for (iter = 0; iter < close.length; iter++) {
+    close[iter].onclick = function () {
+      div = this.parentElement;
       div.style.display = "none";
     };
   }
